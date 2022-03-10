443abd0ae26d13e2976792a25f59e703f2c6b77e
Hieu Vu Duc Nguyen <vdhieu01@gmail.com>
2022-03-10 08:53:55 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+};
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(newFavorite){
+  bestCustomer = newFavorite;
+}
+const leastFavoriteCustomer = "Anna"; 
+function changeLeastFavoriteCustomer(){
+  return leastFavoriteCustomer = "Someone-else"; 
+}
